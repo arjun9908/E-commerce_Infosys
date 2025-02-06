eed28461efe7f5604165c0224a5cf851857ddf25
arjun9908 <nagireddyarjunreddy8@gmail.com>
2025-02-06 09:06:01 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,2 +1,2 @@
 We are learning slenium with java 
-We are adding second point in central to check in the local floder whther this point is updated or not
+We are adding second point in central to check in the local floder whatever this point is updated or not
